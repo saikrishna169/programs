143b8a9a210846b7498f04abd6b09cd289f45202
prince sai kaarishna <96932695+saikrishna169@users.noreply.github.com>
2022-04-29 10:44:16 +0530

    Update and rename from datetime import datetime,timedelta.py to print dates

--- from datetime import datetime,timedelta.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime,timedelta
-start_dt_string=input()
-end_dt_string=input()
-start_dt=datetime.strptime(start_dt_string,'%b %d %Y')
-end_dt=datetime.strptime(end_dt_string,'%b %d %Y')
-number_of_days=(end_dt- start_dt).days
-for i in range (number_of_days+1):
-    print(start_dt+timedelta(days=i))
